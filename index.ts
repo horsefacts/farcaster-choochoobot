34be69d759d0dca10ab27364a9190ded43212f56
horsefacts <webmaster@terminally.online>
2024-02-24 22:25:46 -0500

    add dockerfile

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("Hello from Choochoobot!");
+import { Hex } from "viem";
+import makeTrain from "./lib/makeTrain";
+import sendCast from "./lib/sendCast";
+
+const FID = parseInt(process.env.CHOOCHOOBOT_FID ?? "0");
+const ACCOUNT_PRIVATE_KEY = (process.env.CHOOCHOOBOT_ACCOUNT_PRIVATE_KEY ??
+  "0x00") as Hex;
+
+await sendCast(FID, ACCOUNT_PRIVATE_KEY, makeTrain(), "https://warpcast.com/~/channel/trains");
